c4399bde763c8a36647ba851b5bae1e94792e5b9
Caroline Kenworthy <kenworthyc@gmail.com>
2015-11-20 15:14:58 -0600

    Add anti-spam to form

--- index.php
+++ index.php
@@ -2,19 +2,20 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: kenworthyc';
+    $from = 'From: kenworthyc.github.io';
     $to = 'kenworthyc@gmail.com';
     $subject = 'Hi, Caroline';
+    $human = $_POST['human'];
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-?>
 
-<?php
-if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) {
-        echo '<p>Thanks for your note!</p>';
+    if ($_POST['submit'] && $human == '4') {
+        if (mail ($to, $subject, $body, $from)) {
+        echo '<p>Your message has been sent!</p>';
     } else {
         echo '<p>Something went wrong, go back and try again!</p>';
     }
-}
+    } else if ($_POST['submit'] && $human != '4') {
+    echo '<p>You answered the anti-spam question incorrectly!</p>';
+    }
 ?>
